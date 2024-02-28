ea8049c051853a755c00c1bac9dcfcd9d50f393d
drenzz1 <you@example.com>
2024-02-28 13:58:22 +0100

    Task/task-status Controller

--- src/main/java/com/foo/repository/ProjectRepository.java
+++ src/main/java/com/foo/repository/ProjectRepository.java
@@ -1,8 +1,12 @@
 package com.foo.repository;
 
 import com.foo.entity.Project;
+import com.foo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
 
 public interface ProjectRepository extends JpaRepository<Project,Long> {
     Project findByProjectCode(String code);
+    List<Project> findAllByAssignedManager(User manager);
 }
